6fa5a9eace93c03bd087b6604ab76eb19acefe13
docentsgs <docentsgs@gmail.com>
2019-10-02 09:39:46 +0300

    working on task2,4

--- PS_2/warm-up/task2/script.js
+++ PS_2/warm-up/task2/script.js
@@ -3,11 +3,11 @@ function calcSec() {
   let temp = usr_time.split(':');
   let res = (+temp[0])*3600 + (+temp[1])*60 + (+temp[2]);
 
-  document.getElementById('res3').innerHTML = res;
+  document.getElementById('result_time').innerHTML = res;
 }
 
 function calcTime() {
-  let user_sec = parseInt(document.getElementById('sec_1').value);
+  let user_sec = parseInt(document.getElementById('usr_input').value);
   let hours   = Math.floor(user_sec / 3600);
   let minutes = Math.floor((user_sec - (hours * 3600)) / 60);
   let seconds = Math.floor(user_sec % 60);
@@ -18,5 +18,5 @@ function calcTime() {
   }  if (seconds < 10) {
     seconds = "0" + seconds;
   }
-  document.getElementById('res2').innerHTML = hours + ':' + minutes + ':' + seconds;
+  document.getElementById('result_sec').innerHTML = hours + ':' + minutes + ':' + seconds;
 }
--- PS_2/warm-up/task4/script.js
+++ PS_2/warm-up/task4/script.js
@@ -23,3 +23,4 @@ function makeChessboard() {
     }
   }
 }
+makeChessboard();
