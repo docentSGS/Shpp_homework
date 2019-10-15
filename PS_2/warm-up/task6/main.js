a9b672c046d7a482ebbaab4894977df7d09e829a
docentSGS <docentsgs@gmail.com>
2019-10-15 22:42:15 +0300

    correct names

--- PS_2/warm-up/task6/main.js
+++ PS_2/warm-up/task6/main.js
@@ -1,14 +1,14 @@
 window.onload = function () {
-  let btn = document.getElementById('matchBtn');
-  btn.addEventListener('click', function(event) {
+  let onClickBtn = document.getElementById('matchBtn');
+  onClickBtn.addEventListener('click', function(event) {
     event.preventDefault();
     searchAndMark();
   })
 };
 
 function searchAndMark() {
-  let inputText = document.getElementById('user_text').value;
-  let regExp = new RegExp(document.getElementById('user_regEx').value, 'g');
+  let inputText = document.getElementById('userText').value;
+  let regExp = new RegExp(document.getElementById('userExp').value, 'g');
   let outputText = inputText.replace(regExp, '<mark>$&</mark>');
 
   document.getElementById('result').innerHTML = outputText;
