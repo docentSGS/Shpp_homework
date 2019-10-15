2f31c33434a7f0e0961e902531c6a723791f7c55
docentSGS <docentsgs@gmail.com>
2019-10-15 22:12:22 +0300

    add "event.preventDefault()"

--- PS_2/warm-up/task6/main.js
+++ PS_2/warm-up/task6/main.js
@@ -1,10 +1,15 @@
+window.onload = function () {
+  const button = document.getElementById('matchBtn');
+  button.addEventListener('click', function(event) {
+    event.preventDefault();
+    searchAndMark();
+  })
+};
 
-function searchAndMark() {
-
-  let userText = document.getElementById('user_text').value;
+function searchAndMark() { 
+  let inputText = document.getElementById('user_text').value;
   let regExp = new RegExp(document.getElementById('user_regEx').value, 'g');
-  let outText = userText.replace(regExp, '<mark>$&</mark>');
-  
-  // document.getElementById('result').innerHTML = outText;
-  document.write(outText);
+  let outputText = inputText.replace(regExp, '<mark>$&</mark>');
+
+  document.getElementById('result').innerHTML = outputText;
 }
