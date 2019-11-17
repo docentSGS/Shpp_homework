eb16da7bbb6a9ba566f4e98b7639576b50982dda
docentSGS <docentsgs@gmail.com>
2019-11-17 21:38:06 +0200

    fix issues after first revue, add validate input data

--- PS_2/warm-up/task2/main.js
+++ PS_2/warm-up/task2/main.js
@@ -1,22 +1,41 @@
 function calcSec() {
-  let usr_time = document.getElementById("usr_num").value;
-  let temp = usr_time.split(':');
-  let res = (+temp[0])*3600 + (+temp[1])*60 + (+temp[2]);
+  const usrTime = document.getElementById("usr_num").value;
+  const resultStep2 = document.getElementById('result_time');
+  const timePattern = /^\d{2}:\d{2}:\d{2}$/;
 
-  document.getElementById('result_time').innerHTML = res;
+  if (!usrTime.match(timePattern)) {
+    resultStep2.innerHTML = 'Please, enter correct data';
+    return 0;
+  }
+
+  if (usrTime.match(timePattern)) {
+    let temp = usrTime.split(':');
+    let res = (+temp[0])*3600 + (+temp[1])*60 + (+temp[2]);
+    resultStep2.innerHTML = res + ' seconds';
+  }
 }
 
 function calcTime() {
-  let user_sec = parseInt(document.getElementById('usr_input').value);
-  let hours   = Math.floor(user_sec / 3600);
-  let minutes = Math.floor((user_sec - (hours * 3600)) / 60);
-  let seconds = Math.floor(user_sec % 60);
-  if (hours < 10) {
-    hours   = "0" + hours;
-  }  if (minutes < 10) {
-    minutes = "0" + minutes;
-  }  if (seconds < 10) {
-    seconds = "0" + seconds;
+  const usrSec = parseInt(document.getElementById('usr_input').value);
+  const resultStep1 = document.getElementById('result_sec');
+  const secPattern = /^\d{1,}$/;
+
+  if (!secPattern.test(usrSec)) {
+    resultStep1.innerHTML = 'Please, enter correct data';
+    return 0;
   }
-  document.getElementById('result_sec').innerHTML = hours + ':' + minutes + ':' + seconds;
+
+  if (secPattern.test(usrSec)) {
+    hours   = Math.floor(usrSec / 3600);
+    minutes = Math.floor((usrSec - (hours * 3600)) / 60);
+    seconds = Math.floor(usrSec % 60);
+    if (hours < 10) {
+      hours   = "0" + hours;
+    }  if (minutes < 10) {
+      minutes = "0" + minutes;
+    }  if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+    resultStep1.innerHTML = hours + ':' + minutes + ':' + seconds;
+  }
 }
