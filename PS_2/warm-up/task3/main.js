3c8abb60d3727d639a0379de6a385b8f00fe2937
docentSGS <docentsgs@gmail.com>
2019-11-18 20:47:23 +0200

    fixed bugs after first revue, added validate, corrected algorithm

--- PS_2/warm-up/task3/main.js
+++ PS_2/warm-up/task3/main.js
@@ -1,14 +1,30 @@
-function calcTimeSpan() {
-  let date1 = document.getElementById("usr_dt1").value;
-  let date2 = document.getElementById("usr_dt2").value;
-  let start = new Date(date1);
-  let finish = new Date(date2);
-  let year = finish.getFullYear() - start.getFullYear();
-  let mon = finish.getMonth() - start.getMonth();
-  let day = finish.getDate() - start.getDate();
-  let hour = finish.getHours() - start.getHours();
-  let min = finish.getMinutes() - start.getMinutes();
-  let sec = finish.getSeconds() - start.getSeconds();
+document.getElementById('btnCalc').onclick = function() {
+  
+  const date1 = document.getElementById("usrInput1").value;
+  const date2 = document.getElementById("usrInput2").value;
 
-  document.write("The time span between dates is: " + year +" year "+ mon + " mons " + day + " days " + hour +" hours " + min + " min " + sec + " sec.");
+  if (!date1 || !date2) {
+    document.getElementById('calc_result').innerHTML = 'Please, enter data';
+    return 0;
+  }
+
+  if (date1 >= date2) {
+    calcDateAndTime(date1, date2);
+  } else {
+    calcDateAndTime(date2, date1);
+  }
+}
+
+function calcDateAndTime(num1, num2) {
+  const start = new Date(num1);
+  const finish = new Date(num2);
+  const year = start.getFullYear() - finish.getFullYear();
+  const mon = start.getMonth() - finish.getMonth();
+  const day = start.getDay() - finish.getDay();
+  const hour = Math.abs(start.getHours() - finish.getHours());
+  const min = Math.abs(start.getMinutes() - finish.getMinutes());
+  const sec = start.getSeconds() - finish.getSeconds();
+
+  document.getElementById('calc_result').innerHTML = "The time span between dates is: " + year +" year "+ mon + " mons " + day + " days " + hour + " hours " + min + " min " + sec + " sec.";
+
 }
