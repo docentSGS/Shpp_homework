2905458753f92f12ade37c2ef41d1e9d98c0a664
docentSGS <docentsgs@gmail.com>
2019-11-20 20:32:40 +0200

    fixed misstakes after first revue, added validateempty data, Ipv6, cleared result

--- PS_2/warm-up/task5/main.js
+++ PS_2/warm-up/task5/main.js
@@ -8,30 +8,36 @@ function prepareLinkText() {
   tempArray = tempArray.map(function(e) {
     return e.replace(/\s/g, '');
   });
-
   tempArray = tempArray.map(function(e) {
     return e.replace(/^https?:\/\//, '');
-    });
-
-    let finishedArray = tempArray.map(validUrlAndIp).filter(Boolean);
-    console.log(finishedArray);
-    finishedArray.sort();
-    makeLink(finishedArray);
-  }
+  });
+  let finishedArray = tempArray.map(validUrlAndIp).filter(Boolean);
+  finishedArray.sort();
+  makeLink(finishedArray);
+}
 
 function makeLink(finishedArray) {
-    for (let i = 0; i < finishedArray.length; i++) {
-      let link = document.createElement('a');
-      let linkTitle = document.createTextNode(finishedArray[i]);
-      link.appendChild(linkTitle);
-      link.target = 'blank';
-      link.href = finishedArray[i];
-      link.innerHTML = finishedArray[i] + '<br>';
-      document.body.appendChild(link);
-    }
-  }
+  const result = document.getElementById('result');
+  result.innerHTML = '';
+  if (finishedArray.length===0) {
+    result.innerHTML = 'Please, enter data';
+    return;
+}
 
-function validUrlAndIp(value, index, array){
-    let res = value.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)|(\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b)/gi);
-      return res;
-    }
+  for (let i = 0; i < finishedArray.length; i++) {
+    let link = document.createElement('a');
+    let linkTitle = document.createTextNode(finishedArray[i]);
+    link.appendChild(linkTitle);
+    link.target = 'blank';
+    link.href = finishedArray[i];
+    link.innerHTML = finishedArray[i] + '<br>';
+    result.appendChild(link);
+  }
+}
+
+function validUrlAndIp(value, index, array) {
+  const URL_Ipv4_Ipv6_patern = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)|((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))/gi;
+
+  let outcome = value.match(URL_Ipv4_Ipv6_patern);
+  return outcome;
+}
