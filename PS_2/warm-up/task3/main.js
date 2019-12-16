dbe3291eff8b7c1fe5c650afadfd89f23ae3cc00
docentSGS <docentsgs@gmail.com>
2019-12-16 19:25:57 +0200

    fix dateCalc() algorithm

--- PS_2/warm-up/task3/main.js
+++ PS_2/warm-up/task3/main.js
@@ -1,5 +1,5 @@
 document.getElementById('btnCalc').onclick = function() {
-  
+
   const date1 = document.getElementById("usrInput1").value;
   const date2 = document.getElementById("usrInput2").value;
 
@@ -8,23 +8,24 @@ document.getElementById('btnCalc').onclick = function() {
     return 0;
   }
 
-  if (date1 >= date2) {
-    calcDateAndTime(date1, date2);
+  let startTime = new Date(date1);
+  let finishTime = new Date(date2);
+
+  if (startTime >= finishTime) {
+    calcDateAndTime(startTime, finishTime);
   } else {
-    calcDateAndTime(date2, date1);
+    calcDateAndTime(finishTime, startTime);
   }
 }
 
-function calcDateAndTime(num1, num2) {
-  const start = new Date(num1);
-  const finish = new Date(num2);
-  const year = start.getFullYear() - finish.getFullYear();
-  const mon = start.getMonth() - finish.getMonth();
-  const day = start.getDay() - finish.getDay();
-  const hour = Math.abs(start.getHours() - finish.getHours());
-  const min = Math.abs(start.getMinutes() - finish.getMinutes());
-  const sec = start.getSeconds() - finish.getSeconds();
+function calcDateAndTime(startTime, finishTime) {
+  const year = Math.abs(startTime.getFullYear() - finishTime.getFullYear());
+  const mon = Math.abs(startTime.getMonth() - finishTime.getMonth());
+  const day = Math.abs(startTime.getDate() - finishTime.getDate());
+  const hour = Math.abs(startTime.getHours() - finishTime.getHours());
+  const min = Math.abs(startTime.getMinutes() - finishTime.getMinutes());
+  const sec = Math.abs(startTime.getSeconds() - finishTime.getSeconds());
 
-  document.getElementById('calc_result').innerHTML = "The time span between dates is: " + year +" year "+ mon + " mons " + day + " days " + hour + " hours " + min + " min " + sec + " sec.";
+  document.getElementById('calc_result').innerHTML = "The time span between dates is: " + year + " year " + mon + " mons " + day + " days " + hour + " hours " + min + " min " + sec + " sec.";
 
 }
