f71e65adfca8234480a89d33916e306cbe35e264
docentSGS <docentsgs@gmail.com>
2019-12-16 19:56:47 +0200

    rename some variables, check ipV6-adreses

--- PS_2/warm-up/task5/main.js
+++ PS_2/warm-up/task5/main.js
@@ -2,42 +2,42 @@ const userLinks = document.getElementById('user_input');
 userLinks.onblur = prepareLinkText;
 
 function prepareLinkText() {
-  let data = userLinks.value;
-  let tempArray = data.split(',');
+  let inputData = userLinks.value;
+  let splitedData = inputData.split(',');
 
-  tempArray = tempArray.map(function(e) {
+  intermediateDataArray = splitedData.map(function(e) {
     return e.replace(/\s/g, '');
   });
-  tempArray = tempArray.map(function(e) {
+  intermediateDataArray = intermediateDataArray.map(function(e) {
     return e.replace(/^https?:\/\//, '');
-  });
-  let finishedArray = tempArray.map(validUrlAndIp).filter(Boolean);
-  finishedArray.sort();
-  makeLink(finishedArray);
-}
-
-function makeLink(finishedArray) {
-  const result = document.getElementById('result');
-  result.innerHTML = '';
-  if (finishedArray.length===0) {
-    result.innerHTML = 'Please, enter data';
-    return;
-}
-
-  for (let i = 0; i < finishedArray.length; i++) {
-    let link = document.createElement('a');
-    let linkTitle = document.createTextNode(finishedArray[i]);
-    link.appendChild(linkTitle);
-    link.target = 'blank';
-    link.href = finishedArray[i];
-    link.innerHTML = finishedArray[i] + '<br>';
-    result.appendChild(link);
+    });
+    let finishedData = intermediateDataArray.map(validUrlAndIp).filter(Boolean);
+    finishedData.sort();
+    makeLink(finishedData);
   }
-}
 
-function validUrlAndIp(value, index, array) {
-  const URL_Ipv4_Ipv6_patern = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)|((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))/gi;
+  function makeLink(finishedData) {
+    const result = document.getElementById('result');
+    result.innerHTML = '';
+    if (finishedData.length===0) {
+      result.innerHTML = 'Please, enter data';
+      return;
+    }
 
-  let outcome = value.match(URL_Ipv4_Ipv6_patern);
-  return outcome;
-}
+    for (let i = 0; i < finishedData.length; i++) {
+      let link = document.createElement('a');
+      let linkTitle = document.createTextNode(finishedData[i]);
+      link.appendChild(linkTitle);
+      link.target = 'blank';
+      link.href = finishedData[i];
+      link.innerHTML = finishedData[i] + '<br>';
+      result.appendChild(link);
+    }
+  }
+
+  function validUrlAndIp(value, index, array) {
+    const URL_Ipv4_Ipv6_patern = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)|((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))/gi;
+
+      let outcome = value.match(URL_Ipv4_Ipv6_patern);
+      return outcome;
+    }
